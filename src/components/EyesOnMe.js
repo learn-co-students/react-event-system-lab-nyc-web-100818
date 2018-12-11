51a3652fdd42aed1b9160e3f7696f8af9cc92b24
Veraprot <vera.protopopova@gmail.com>
2018-12-11 17:01:37 -0500

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,30 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  handleBlur = () => { 
+    console.log('Hey! Eyes on me!');
+     
+  }
+
+  handleFocus = () => {
+    console.log('Good!');
+  }
+
+  render() {
+    return(
+        <button 
+          type="password" 
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
+        ></button>
+
+    )
+  }
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,26 @@
 // Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  handleKeyUp = (event) => {
+    console.log('Entering password...');
+  }
+
+  render() {
+    return(
+      <form action="">
+        <input 
+          type="password" 
+          onKeyUp={this.handleKeyUp}
+        />
+      </form>
+
+    )
+  }
+}
+
+export default Keypad

93fba4a420f87e9aeff25196929c0db26f970c0d
skbtrc <skbsturac@live.com>
2018-12-12 15:19:43 -0500

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
 // Code Keypad Component Here
+// Code EyesOnMe Component Here
+import React from 'react'
+class Keypad extends React.Component {
+
+  logInput = (event) => {
+		console.log("Entering password...")
+	}
+
+  render(){
+    return <input type="password" onKeyUp={(event)=>this.logInput(event)}/>
+  }
+}
+export default Keypad
